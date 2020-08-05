38dd60ca0bfda2865f40204e22054d0554ac75ec
CarlosRPEvertsz <carlosrpevertsz@gmail.com>
2020-08-04 20:06:01 -0400

    Add project files.

---
+++ Proyecto3_Diplomado_Web_MVC_UASD_Registro/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto3_Diplomado_Web_MVC_UASD_Registro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index() {
+            var coches = new List<Models.Coche>
+            {
+                    new Models.Coche { Tipo = "Jeep", Marca = "BMW", Modelo = "X6" , Color = "Azul" },
+                    new Models.Coche { Tipo = "Auto", Marca = "Mercedes", Modelo = "A200" , Color = "Blanco" },
+                    new Models.Coche { Tipo = "Jeep", Marca = "Porsche", Modelo = "Cayenne" , Color = "Negro" },
+                    new Models.Coche { Tipo = "Auto", Marca = "Ford", Modelo = "Mustang" , Color = "Rojo" }
+            };
+            return View(coches);
+        }
+    }
+}
